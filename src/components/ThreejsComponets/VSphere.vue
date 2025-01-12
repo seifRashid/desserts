81739235293f371ebcac16ccfbc7240653ee3da3
seifRashid <rseif1218@gmail.com>
2025-01-12 05:10:18 +0300

    enable 3d the object to resize to screen size

--- src/components/ThreejsComponets/VSphere.vue
+++ src/components/ThreejsComponets/VSphere.vue
@@ -6,32 +6,36 @@
 <script setup>
 import * as THREE from 'three'
 import { onMounted, ref } from 'vue'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const canvas = ref(null)
 
 onMounted(() => {
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  // orthographic camera
+  const camera = new THREE.OrthographicCamera(-10, 10, 10, -10, 0.1, 1000)
   camera.position.z = 5
 
   const renderer = new THREE.WebGLRenderer({ canvas: canvas.value })
   // instantiate controls with the canvas ref
   const controls = new OrbitControls(camera, canvas.value)
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  // renderer.setSize(window.innerWidth, window.innerHeight)
 
-  const geometry = new THREE.SphereGeometry(1, 32, 32)
+  // const geometry = new THREE.SphereGeometry(1, 32, 32)
+  const geometry = new THREE.BoxGeometry(2, 2, 2)
   const material = new THREE.MeshBasicMaterial({ color: 'green', wireframe: true })
   const sphere = new THREE.Mesh(geometry, material)
   scene.add(sphere)
 
   function animate() {
-    requestAnimationFrame(animate)
-
+    camera.setSize(window.innerWidth / window.innerHeight)
+    renderer.setSize(window.innerWidth, window.innerHeight)
     sphere.rotation.x += 0.01
     sphere.rotation.y += 0.01
- controls.update()
+    controls.update()
     renderer.render(scene, camera)
+    requestAnimationFrame(animate)
   }
   animate()
 })
